3e3ed3b740060a5037e180e9aa0c2861513e63cc
Amitai <git@amitai.ga>
2019-10-20 03:28:33 +0100

    improved compatibility for non-java users

--- even.py
+++ even.py
@@ -1,13 +1,17 @@
 import subprocess
+import sys
 
 def isEven(x):
 	while True:
-		if x == 0:
+		if x == '0':
 			return True
-		elif x == 1:
+		elif x == '1':
 			return False
 		print(x)
-		x = int(subprocess.check_output(['java', 'SquareInt', str(x)]))
+                try:
+		    x = subprocess.check_output(['java', 'SquareInt', x])
+                except:
+                    return "u dont have java"
 
 if __name__ == '__main__':
-	print(isEven(input()))
+	print(isEven(sys.argv[1]))
