ae2f6bf5cbe39939f4385bb9c6083d177fc367c8
MarchiT <mrtihova@gmail.com>
2019-11-01 22:10:39 +0000

    ported to c

--- even.py
+++ even.py
@@ -2,16 +2,14 @@ import subprocess
 import sys
 
 def isEven(x):
-	while True:
-		if x == '0':
-			return True
-		elif x == '1':
-			return False
-		print(x)
-                try:
-		    x = subprocess.check_output(['java', 'SquareInt', x])
-                except:
-                    return "u dont have java"
+    while True:
+        if x == 0:
+            return True
+        elif x == 1:
+            return False
+        print(x)
+        x = int(subprocess.check_output(['./square', str(x)]))
+
 
 if __name__ == '__main__':
-	print(isEven(sys.argv[1]))
+    print(isEven(int(sys.argv[1])))
---
+++ square.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  int x = atoi(argv[1]);
+  printf("%d", x * x);
+  return 0;
+}
